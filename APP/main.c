a735f406117d3771634beaa50feca2885269ab65
Ali Shawki <Ali.Shawki@ext.mahle.com>
2022-11-25 23:37:12 +0200

    REQ_1

--- APP/main.c
+++ APP/main.c
@@ -1,10 +1,23 @@
 
-
+char arr[]={'a','t','h','y','r'};
 
 
 int main()
 {
-	
+	char i = 0 , j = 0 , temp;
+
+	for(i = 0 ; i < 5-1 ; i++)
+	{
+		for(j = i+1 ; j<5 ; j++)
+		{
+			if(arr[j] > arr[i])
+			{
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
 	
 	while(1)
 	{
