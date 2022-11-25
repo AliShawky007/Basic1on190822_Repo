4d8cbc3836cfdef97534f67429de5e14be674541
Ali Shawki <Ali.Shawki@ext.mahle.com>
2022-11-25 23:37:45 +0200

    REQ_2

--- APP/main.c
+++ APP/main.c
@@ -8,15 +8,7 @@ int main()
 
 	for(i = 0 ; i < 5-1 ; i++)
 	{
-		for(j = i+1 ; j<5 ; j++)
-		{
-			if(arr[j] > arr[i])
-			{
-				temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-			}
-		}
+		
 	}
 	
 	while(1)
