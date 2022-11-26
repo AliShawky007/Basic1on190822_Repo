dd22eb93c6750ef5e56c1355fa2844b96fbe0973
“Ahmed <ahmedabdelazizalsayed@gmail.com>
2022-11-26 13:47:09 -0700

    implementfunction

--- APP/main.c
+++ APP/main.c
@@ -4,6 +4,28 @@
 char* Get_LargestFiveChars(char* arr , int s)
 {
 	//todo
+	uint8 i ,index,j,sort_complet=1,temp=0;
+	index=size;
+	for (i=0;i<size-1;i++)
+	{
+
+		for (j=0;j<index-1;j++)
+		{
+			if (arr[j]>arr[j+1])
+			{
+				temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+				sort_complet=0;
+			}
+		}
+		index--;
+		if (sort_complet==1)
+		{
+			break;
+		}
+	}
+	
 }
 
 
