7ccd60c7cb35e25ee0ec7a2bf5ea9c3c7a8a0576
YoussefSamir9603 <youssef9603@gmail.com>
2022-11-26 23:08:45 +0200

    v1

--- APP/main.c
+++ APP/main.c
@@ -1,21 +1,49 @@
 
 
 
-char* Get_LargestFiveChars(char* arr , int s)
+void main (void)
 {
-	//todo
-}
+   PORT_voidInit();
+   CLCD_voidInit();
 
 
-int main()
-{
-	
-	char arr[]={'S','A','P','O','N','B','C','O'};
-	
-	
+    uint8 Local_u8Array[8]={'S','A','P','O','N','B','C','O'};
+   	sint8 Local_s8Counter1=0;
+   	sint8 Local_s8Counter2=0;
+   	uint8 Local_u8Temp=0;
+
+   	for (Local_s8Counter1=0;Local_s8Counter1<7;Local_s8Counter1++)
+   	{
+   		for (Local_s8Counter2=Local_s8Counter1+1;Local_s8Counter2<8;Local_s8Counter2++)
+   		{
+   			if (Local_u8Array[Local_s8Counter1]>Local_u8Array[Local_s8Counter2])
+   			{
+   				Local_u8Temp=Local_u8Array[Local_s8Counter1];
+   				Local_u8Array[Local_s8Counter1]=Local_u8Array[Local_s8Counter2];
+   				Local_u8Array[Local_s8Counter2]=Local_u8Temp;
+
+   			}
+   		}
+
+   	}
+
+
+   	CLCD_voidGoToXY(16,0);
+   	for(Local_s8Counter1=16;Local_s8Counter1>=11;Local_s8Counter1--)
+   	{
+   		CLCD_voidGoToXY(Local_s8Counter1,1);
+   		CLCD_voidSendData(Local_u8Array[Local_s8Counter1-8]);
+   	}
+
+
+
+
+
+
 	while(1)
 	{
+
+
+
 	}
-	
-	return 0;
 }
