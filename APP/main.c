b9d2952860b21f964c0fec9086c073f3487f72f4
Karim_Hweidy <karimhweidy1997@gmail.com>
2022-11-26 22:50:20 +0200

    task 1

--- APP/main.c
+++ APP/main.c
@@ -4,6 +4,24 @@
 char* Get_LargestFiveChars(char* arr , int s)
 {
 	//todo
+	int i=0;
+	int j=0
+    int temp=0;
+
+		for (i=0;i<7;i++)
+		{
+			for (j=i+1;j<8;j++)
+			{
+				if (arr[i]>arr[j])
+				{
+					temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+
+				}
+			}
+
+		}
 }
 
 
