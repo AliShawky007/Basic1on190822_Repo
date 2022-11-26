0ae7264281fad1bda350c29d8578aa322c780379
“Mahmoud <mahmoud.a.eltawil@gmail.com>
2022-11-26 21:45:48 +0100

    Add sorting function

--- APP/main.c
+++ APP/main.c
@@ -1,9 +1,34 @@
 
+#include "STD_TYPES.h"
+#include "util/delay.h"
 
+#include "DIO_interface.h"
+#include "PORT_interface.h"
 
-char* Get_LargestFiveChars(char* arr , int s)
+#include "CLCD_interface.h"
+void Get_LargestFiveChars(char* arr , int s)
 {
-	//todo
+	int i;
+	int j;
+	int temp;
+	int sorting_done = 1;
+	for (i = 0; i < s-1; ++i)
+	{
+		sorting_done = 1;
+		for (j = 0; j < s - 1 - i; j++)
+		{
+			if (arr[j] > arr[j+1])
+			{
+				temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+				sorting_done  = 0;
+			}
+		}
+		if(sorting_done == 1) break;
+	}
+
+	return;
 }
 
 
@@ -11,7 +36,13 @@ int main()
 {
 	
 	char arr[]={'S','A','P','O','N','B','C','O'};
-	
+	Get_LargestFiveChars(arr, 8);
+	PORT_voidInit();
+	CLCD_voidInit();
+	for (counter = 0; counter < 5; counter++)
+	{
+		CLCD_voidSendData(Arr[counter]);
+	}
 	
 	while(1)
 	{
