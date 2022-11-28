364a3a90be1cf9a227e8f7001fd73187a789cdfc
oomarmohamedd <oomarmohamedd@gmail.com>
2022-11-28 02:15:44 +0200

    Omar

--- APP/main.c
+++ APP/main.c
@@ -3,7 +3,23 @@
 
 char* Get_LargestFiveChars(char* arr , int s)
 {
-	//todo
+	sint8 Local_s8Counter1=0;
+		sint8 Local_s8Counter2=0;
+		uint8 Local_u8Temp=0;
+
+		for (Local_s8Counter1=0;Local_s8Counter1<7;Local_s8Counter1++)
+		{
+			for (Local_s8Counter2=Local_s8Counter1+1;Local_s8Counter2<8;Local_s8Counter2++)
+			{
+				if (arr[Local_s8Counter1]>arr[Local_s8Counter2])
+				{
+					Local_u8Temp=arr[Local_s8Counter1];
+					arr[Local_s8Counter1]=arr[Local_s8Counter2];
+					arr[Local_s8Counter2]=Local_u8Temp;
+				}
+			}
+		}
+
 }
 
 
